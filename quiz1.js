4089d945a2f8a1d7816c97234f695edb8f411618
Thomas Spuridon Paschalis P2019110 <p19pasc@ionio.gr>
2022-01-02 16:09:20 +0200

    Add files via upload

--- quiz1.js
+++ quiz1.js
@@ -3,59 +3,59 @@ function submit(){
     var c=0;/*initialization of the counter for the correct answers */ 
     /* each variable below is initialized with 1 of 4 values that are given in the quiz1.html(in every div section), based on the preference of the user*/ 
     /* each variable can get values {a,b,c,d}*/
-    var q1=document.quiz.question1.value;
-    var q2=document.quiz.question2.value;
-    var q3=document.quiz.question3.value;
-    var q4=document.quiz.question4.value;
-    var q5=document.quiz.question5.value;
-    var q6=document.quiz.question6.value;
+    var answer1=document.quiz.question1.value;
+    var answer2=document.quiz.question2.value;
+    var answer3=document.quiz.question3.value;
+    var answer4=document.quiz.question4.value;
+    var answer5=document.quiz.question5.value;
+    var answer6=document.quiz.question6.value;
     var flag=false; /* initialization of flag with false value. In case the user has answered one of the six questions that we have in quiz1.html
     the message with his score and the repeat button should be displayed*/
 
     /* In every if's first case that we see is the correct answer of the corresponding question, so the counter should be increased by 1
         else if the user has chosen a wrong answer our flag variable gets the true value */
-    if (q1=="c"){ 
+    if (answer1=="c"){ 
         c++;
-    }else if(q1=="b" || q1=="a" || q1=="d")
+    }else if(answer1=="b" || answer1=="a" || answer1=="d")
     {
         flag = true;
     }
 
-    if (q2=="d"){
+    if (answer2=="d"){
         c++;
-    }else if(q2=="a" || q2=="c" || q2=="b")
+    }else if(answer2=="a" || answer2=="c" || answer2=="b")
     {
         flag = true;
     }
 
-    if (q3=="c"){
+    if (answer3=="c"){
         c++;
     }
-    else if(q3=="a" || q3=="b" || q3=="d")
+    else if(answer3=="a" || answer3=="b" || answer3=="d")
     {
         flag = true;
     }
 
-    if (q4=="a")
+    if (answer4=="a")
     {
         c++;
     }
-    else if(q4=="d" || q4=="b" || q4=="c")
+    else if(answer4=="d" || answer4=="b" || answer4=="c")
     {
         flag = true;
     }
     
-    if (q5=="d"){
+    if (answer5=="d"){
         c++;
     }
-    else if(q5=="a" || q5=="b" || q5=="c")
+    else if(answer5=="a" || answer5=="b" || answer5=="c")
     {
         flag = true;
     }
     if (q6=="b"){
         c++;
     }
-    else if(q6=="a" || q6=="d" || q6=="c")
+    else if(answer6=="a" || answer6=="d" || answer6=="c")
     {
         flag = true;
     }
